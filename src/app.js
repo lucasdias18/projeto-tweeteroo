e48b81c35f177834d68263e3dc76935fd4df3b3b
Lucas Dias <lucasdias.ld10@gmail.com>
2023-01-09 13:36:04 -0300

    implementando post tweet

--- src/app.js
+++ src/app.js
@@ -23,9 +23,20 @@ app.post('/sign-up', (req, res) => {
 app.post('/tweets', (req, res) => {
     const name = req.body.username
     const tt = req.body.tweet
+    if(!name || !tt) {
+        return res.status(400).send('Todos os campos são obrigatórios!')
+    }
+    const verificacao = users.find((n => n.username === name))
+    if (verificacao === undefined) {
+        return res.status(401).send('Faça o cadastro primeiro!')
+    }
     const tweet = {username: name, tweet: tt}
     tweet.push(tweets)
     res.send('Ok!')
+})
+
+app.get('/tweets', (req, res) => {
+    console.log('feito!')
 })
 
 app.listen(5000, () => console.log('servidor subiu namoral!!'));
